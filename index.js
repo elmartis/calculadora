f39bc4165e43d43e0d21f87269bb7981f35cde46
juan david martin <54649533+elmartis@users.noreply.github.com>
2020-12-18 14:26:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var c;
+function el1(){
+    calc.text.value+="1";
+}
+function el2(){
+    calc.text.value+="2";
+}
+function el3(){
+    calc.text.value+="3";
+}
+function elsum(){
+    calc.text.value+="+";
+}
+function el4(){
+    calc.text.value+="4";
+}
+function el5(){
+    calc.text.value+="5";
+}
+function el6(){
+    calc.text.value+="6";
+}
+function elrest(){
+    calc.text.value+="-";
+}
+function el7(){
+    calc.text.value+="7";
+}
+function el8(){
+    calc.text.value+="8";
+}
+function el9(){
+    calc.text.value+="9";
+}
+function elmulti(){
+    calc.text.value+="*";
+}
+function eldivi(){
+    calc.text.value+="/";
+}
+function ellimp(){
+    calc.text.value="";
+}
+function el0(){
+    calc.text.value+="0";
+}
